7354833177a5bef6d08735c280061e123126afd7
saruatchu <sarumathiayyappan@gmail.com>
2018-09-03 12:33:16 +0530

    Create Integer.java

---
+++ Integer.java
@@ -0,0 +1,18 @@
+# Integer
+import java.io.*;
+import java.util.*;
+class Integer
+{
+public static void main(String args[])
+{
+int count=0;
+scanner sc=new scanner(Sytem.in)
+int n=sc.nextInt();
+while(n!=0);
+{
+n=n/10;
+count++;
+}
+System.out.println(count);
+}
+}
